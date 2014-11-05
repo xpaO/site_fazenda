e1bfba0cdcf5a6d9d9ba7b784fc3d1ea68a27cc1
Henrique <henrique.coura@live.com>
2014-11-05 19:57:30 -0200

    cabecalho funcional

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout test" do
+    get root_path
+    assert_template "paginas_basicas/inicio"
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", localizacao_path
+    assert_select "a[href=?]", contato_path
+  end
+end
